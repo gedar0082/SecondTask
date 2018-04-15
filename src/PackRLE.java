f513ad980ec345ae6c829baf9135dbafb6305e61
gedar <gedar0082@gmail.com>
2018-04-15 23:49:29 +0300

    Готовы: -представление, что надо сделать; -базовая логика работы класса упаковки PackZip Не готовы: -работа с флагами; -связь классов; -работа класса распаковки; -тесты;

--- src/PackRLE.java
+++ src/PackRLE.java
@@ -1,5 +1,14 @@
+import java.io.*;
+
 public class PackRLE {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws Exception {
+        BufferedReader input = new BufferedReader(new FileReader("input.txt"));
+
+        int symbol = input.read();
+        while (symbol != -1) {
+            char c = (char) symbol;
+            symbol = input.read();
+            System.out.println(c);
+        }
     }
 }
