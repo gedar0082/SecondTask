308e17549bd792d9869468a91243c9824ae20de3
gedar <gedar0082@gmail.com>
2018-04-20 18:37:21 +0300

    ещё не готовы тесты

--- src/PackRLE.java
+++ src/PackRLE.java
@@ -1,14 +1,35 @@
 import java.io.*;
-
+import org.kohsuke.args4j.*;
 public class PackRLE {
-    public static void main(String[] args) throws Exception {
-        BufferedReader input = new BufferedReader(new FileReader("input.txt"));
+    @Option(name = "-z", usage = "Packing")
+    private boolean packFlag;
 
-        int symbol = input.read();
-        while (symbol != -1) {
-            char c = (char) symbol;
-            symbol = input.read();
-            System.out.println(c);
+    @Option(name="-u", usage = "Unpacking")
+    private boolean unpackFlag;
+
+    @Option(name="-out", usage = "Output file")
+    private String outputFile;
+
+    @Argument(usage = "Input file", required = true)
+    private String inputFile;
+
+    public static void main(String[] args) {
+        new PackRLE().launch(args);
+    }
+    private void launch(String[] args) {
+        CmdLineParser parser = new CmdLineParser(this);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println(e.getMessage());
         }
+        Packer packer = new Packer(packFlag, unpackFlag, outputFile, inputFile);
+        try{
+            packer.start();
+        }catch(IOException e){
+            System.err.println(e.getMessage());
+        }
+
+
     }
 }
---
+++ src/Packer.java
@@ -0,0 +1,88 @@
+import java.io.*;
+
+public class Packer {
+    private boolean packFlag;
+    private boolean unpackFlag;
+    private String outputFile;
+    private String inputFile;
+    BufferedWriter out;
+    BufferedReader in;
+    public Packer(boolean packFlag, boolean unpackFlag, String outputFile, String inputFile) {
+        this.packFlag = packFlag;
+        this.unpackFlag = unpackFlag;
+        this.outputFile = outputFile;
+        this.inputFile = inputFile;
+    }
+    public void start() throws IOException {
+        in = new BufferedReader(new FileReader(inputFile));
+        if (outputFile == null) {
+            out = new BufferedWriter(new FileWriter(inputFile + ".rle"));
+        } else {
+            out = new BufferedWriter(new FileWriter(outputFile));
+        }
+        if (packFlag && unpackFlag) throw new IllegalArgumentException();
+        if (packFlag) {
+            pack();
+        } else if (unpackFlag) {
+            unpack();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void pack() throws IOException {
+        int symbol = in.read();
+        int saveSymbol;
+        int count = 1;
+        while (symbol != -1) {
+            saveSymbol = symbol;
+            symbol = in.read();
+            if(saveSymbol == symbol){
+                count++;
+            }
+            else{
+                if(count > 1){
+                    out.append(count + "");
+                    out.append((char)saveSymbol + "");
+                    count = 1;
+                }else{
+                    out.append((char)saveSymbol + "");
+                    count = 1;
+                }
+            }
+        }
+        in.close();
+        out.close();
+    }
+
+    private void unpack() throws IOException {
+        int symbol = in.read();
+        int count = 0;
+        while (symbol != -1){
+            if(symbol >48 & symbol< 58){
+                count = count * 10 + symbol - 48;
+            }else{
+                if(symbol < 33){
+                    out.append((char)symbol);
+                    count = 0;
+                }else{
+                    if(count == 0) count = 1;
+                    for(int c = count; c >0; c--){
+                        out.append((char)symbol);
+                    }
+                    count = 0;
+                }
+            }
+            symbol = in.read();
+        }
+        in.close();
+        out.close();
+    }
+}
+
+
+
+
+
+
+
